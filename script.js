35251c31572480df082c92c0021c8add02e72a94
jssckbl <jessicawoudsma@gmail.com>
2020-04-19 13:31:13 -0500

    checkRequired loops through the array of each input

--- script.js
+++ script.js
@@ -24,31 +24,17 @@ function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+// check required fields
+
+function checkRequired(inputArray) {
+  inputArray.forEach(function(input) {
+    console.log(input);
+  });
+}
 
 // need an event listener for the submit button on the form
+// we can pass in an array of inputs we want to check
 form.addEventListener("submit", function(e) {
   e.preventDefault();
-  console.log("submit");
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-  if (email.value === "") {
-    showError(email, "email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "email is not valid");
-  } else {
-    showSuccess(email);
-  }
-  if (password.value === "") {
-    showError(password, "password is required");
-  } else {
-    showSuccess(password);
-  }
-  if (password2.value === "") {
-    showError(password2, "same password is required");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 });
